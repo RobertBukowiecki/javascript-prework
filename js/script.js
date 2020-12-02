1c577789cbe67e108ceeaf270e7c3a3d2c0d9d82
Robert Bukowiecki <rob.ecki19@gmail.com>
2020-12-02 13:20:44 +0100

    add playerMove script

--- js/script.js
+++ js/script.js
@@ -11,5 +11,21 @@ if(randomNumber == 1){
 } else if (randomNumber == 3) {
     computerMove = 'nożyce'
 }
+l
+et playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+  } else if (playerInput == '2'){
+    playerMove = 'papier';
+  } else if (playerInput == '3'){
+    playerMove = 'nożyce';
+  } 
+
+printMessage('Twój ruch to: ' + playerMove);
 printMessage('Mój ruch to: ' + computerMove);
+
